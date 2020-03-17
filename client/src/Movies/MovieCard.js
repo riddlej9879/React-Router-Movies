951dc8115eaca2060e7efcf381ce45786f61f717
Josh Riddle <riddlej9879@yahoo.com>
2020-03-17 18:54:26 -0500

    Not MVP yet

--- client/src/Movies/MovieCard.js
+++ client/src/Movies/MovieCard.js
@@ -1,7 +1,10 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const MovieCard = props => {
-  return;
+  return (
+    <Link to={`/Movie/${movie.id}`}></Link>
+  );
 };
 
 export default MovieCard;
